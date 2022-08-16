21de5814345533efd45ea405981d8659196463b9
PrachetShah <prachetshah25@gmail.com>
2022-08-16 14:00:21 +0530

    completed storing system

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
+import os
 
-app = Flask(__name__, static_url_path='/static')
- 
+UPLOAD_FOLDER = os.getcwd()+'\static\submitted'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        return render_template('index.html', var1=request.form['image'])
+        img = request.files['image']
+        if img:
+            img_loc = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(img.filename))
+            img.save(img_loc)
+
+        return render_template('index.html', var1=img.filename)
     return render_template('index.html')
  
 if __name__ == '__main__':
